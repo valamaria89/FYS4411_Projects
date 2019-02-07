a64efa3ffefd852434300e01425a88d3d486c845
valamaria89 <vala.maria89@gmail.com>
2019-02-07 17:51:19 +0100

    Added IMS

--- Project1/project/methods.cpp
+++ Project1/project/methods.cpp
@@ -161,4 +161,157 @@ double local_energy_analytic(Matrix r, double alpha, int dim, int numOfPart){
     return eloc;
 }
 
+void QuantumForce(Matrix r, Matrix QForce, int dim, int numOfPart, double alpha){
+    Matrix rplus(numOfPart, dim), rminus(numOfPart, dim);
+    for (int i = 0; i < numOfPart; i++){
+             for (int j = 0; j < dim; j++){
+                 rplus.set_Elem(i,j, r.get_Elem(i,j));
+                 rminus.set_Elem(i, j, r.get_Elem(i,j));
+                 radsq  += r.get_Elem(i,j)*r.get_Elem(i, j);
+             }
+    }
+    double wfold = wavefunction(r, alpha, dim, numOfPart);
+    double  wfminus = 0, wfplus = 0;
+
+    for (int i = 0; i < numOfPart; i++){
+             for (int j = 0; j < dim; j++){
+                 rplus.set_Elem(i,j, r.get_Elem(i,j)+h);
+                 rminus.set_Elem(i, j, r.get_Elem(i,j)-h);
+                 wfminus = wavefunction(rminus, alpha, dim, numOfPart);
+                 wfplus  = wavefunction(rplus, alpha, dim, numOfPart);
+                 QForce.set_Elem(i, j, (wfplus-wfminus)/(wfold*h));
+                 rplus.set_Elem(i,j, r.get_Elem(i,j));
+                 rminus.set_Elem(i, j, r.get_Elem(i,j));
+
+
+             }}
+
+
+
+
+
+}
+
+//The wave function without interaction
+double wavefunction(Matrix r, double alpha, int dim, int NumOfPart){
+
+      double wf=0, r_sqr=0;
+
+      for (int i = 0; i < NumOfPart; i++) {
+        for (int j = 0; j < dim; j++) {
+          r_sqr  += r.get_Elem(i,j)*r.get_Elem(i,j);
+        }
+      }
+      wf = exp(-alpha*r_sqr) ;
+      return wf;
+    }
+
+
+void mc_sampling_IMS( double timestep, int dim, int numOfPart, int numMCCycles, int numVar,  double *Etot, double *Etot2, int ind,double alpha, double deltaAlpha)
+{
+//The variational parameter
+       // double alpha=0.50, deltaAlpha=0.01;
+        double wfnew, wfold, eng, eng2, delta_e;
+        int accept;
+
+//Here we set the old and the new positions to zero (2d matrices [number of particles, dimension])
+        Matrix rold(numOfPart, dim);
+        Matrix rnew(numOfPart, dim);
+        Matrix newQForce(numOfPart, dim);
+        Matrix oldQForce(numOfPart, dim);
+
+
+      for (int i = 0; i < numOfPart; i++){
+               for (int j = 0; j < dim; j++){
+                   rold.set_Elem(i, j, 0);
+                   rnew.set_Elem(i ,j, 0);
+                   newQForce.set_Elem(i,j,0);
+                   oldQForce.set_Elem(i, j, 0);
+
+               }
+           }
+
+      random_device rd;
+      mt19937_64 gen(rd());
+      uniform_real_distribution<double> RNG(0.0,1.0);
+      normal_distribution<double> ND(0.0,1.0);
+
+//The loop over the variational parameter
+        for (int var=0; var<=numVar; var++) {
+            eng = 0;
+            eng2 = 0;
+            accept =0;
+            delta_e=0;
+
+ //  initial trial position
+        for (int i = 0; i < numOfPart; i++) {
+             for (int j = 0; j < dim; j++){
+                   rold.set_Elem( i, j, sqrt(timestep)*(ND(gen)));
+                 }
+           }
+
+ // initial trial WF
+    wfold = wavefunction(rold, alpha, dim, numOfPart);
+    QuantumForce(rold, oldQForce, dim, numOfPart, alpha);
+
+
+ //The MC Cycle
+    for (int cycl = 1; cycl <= numMCCycles; cycl++) {
+
+ // We set the new position
+
+
+        // double rsq=0;
+      for (int i = 0; i < numOfPart; i++) {
+           for (int j = 0; j < dim; j++){
+               rnew.set_Elem(i, j, rold.get_Elem(i,j)+sqrt(timestep)*(ND(gen)+0.5*oldQForce.get_Elem(i, j)*timestep));
+               }
+         }
+    wfnew = wavefunction(rnew, alpha, dim, numOfPart);
+    QuantumForce(rnew, newQForce, dim, numOfPart, alpha);
+    double GreenFunction = 0;
+     for (int i = 0; i < numOfPart; i++) {
+         for (int j = 0; j < dim; j++){
+
+             GreenFunction = 0.5(oldQForce.get_Elem(i,j) + newQForce.get_Elem(i,j))*(0.25*timestep(oldQForce.get_Elem(i, j)-newQForce.get_Elem(i, j))+rold.get_Elem(i, j) - rnew.get_Elem(i,j));
+         }
+     }
+     GreenFunction = exp(GreenFunction);
+
+ // Metropolis-Hastings test
+
+    if(RGN(gen) < GreenFunction*wfnew*wfnew/(wfold*wfold) ) {
+        for (int i = 0; i < numOfPart; i++) {
+             for (int j = 0; j < dim; j++){
+                    rold.set_Elem(i, j, rnew.get_Elem(i,j));
+                 }
+           }
+  wfold=wfnew;
+  accept++;
+    }
+
+ // compute local energy
+    if (ind==1){
+        delta_e = local_energy_analytic(rold, alpha, dim, numOfPart);
+    }
+    else{
+        delta_e = local_energy_num(rold, alpha, wfold, dim, numOfPart);
+    }
+// update energies
+    eng  += delta_e;
+    eng2 += delta_e*delta_e;
+
+         }
+// end of loop over MC trials
+    Etot[var] = eng/numMCCycles;
+   // cout<< "enrg= "<<eng<<endl;
+    Etot2[var] = eng2/numMCCycles;
+cout << " " << accept <<endl;
+  //Increase the variational parameter
+        alpha += deltaAlpha;
+ }    // end of loop over variational  steps
+
+ }
+
+
 
